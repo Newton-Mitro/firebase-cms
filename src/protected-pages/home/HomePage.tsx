136331de021325627b64cefea8c0d6b09a5a5e51
Newton-Mitro <newtonmitro@gmail.com>
2024-06-02 18:43:17 +0600

    some change

--- src/protected-pages/home/HomePage.tsx
+++ src/protected-pages/home/HomePage.tsx
@@ -3,6 +3,7 @@ import {
   BarElement,
   CategoryScale,
   Chart as ChartJS,
+  Colors,
   Legend,
   LineElement,
   LinearScale,
@@ -11,7 +12,6 @@ import {
   Title,
   Tooltip,
 } from "chart.js";
-import faker from "faker";
 import { Bar, Doughnut, Line, PolarArea } from "react-chartjs-2";
 
 ChartJS.register(
@@ -24,6 +24,7 @@ ChartJS.register(
   ArcElement,
   Title,
   Tooltip,
+  Colors,
   Legend
 );
 
@@ -37,25 +38,22 @@ export const options = {
       display: true,
       text: "Chart.js Line Chart",
     },
+    colors: {
+      forceOverride: true,
+    },
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+const labels = ["Pages", "Posts", "Notices", "Events", "Users"];
 
 export const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      label: "Pages",
+      data: [6, 150, 50, 10, 10],
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
   ],
 };
